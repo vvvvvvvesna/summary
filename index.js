b5f1178c96ddeb5b97a39bc578960aed16e17081
Kiryl Fedarenkau <k.fedarenkau@reply.com>
2019-09-09 10:06:33 +0300

    add timers

---
+++ index.js
@@ -0,0 +1,34 @@
+function getDifference() {
+    var date1 = new Date('7/11/2010');
+    var date2 = new Date('12/12/2010');
+    return date2.getDate() - date1.getDate();
+}
+
+function getCount(start, id, final) {
+    if (final) {
+        var distance = final - start;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById(id).innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+    } else {
+        var now = new Date().getTime();
+        var distance = now - start;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById(id).innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+    }
+}
+
+const startWMReply = new Date("Jan 23, 2019 10:00:00").getTime();
+const startTaex = new Date("Dec 20, 2018 10:00:00").getTime();
+const finalTaex = new Date("Jul 23, 2019 10:00:00").getTime();
+getCount(startTaex, "taex", finalTaex);
+setInterval(function () {
+    getCount(startWMReply, "wm-reply");
+}, 1000)
