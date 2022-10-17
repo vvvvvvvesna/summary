a87f9daf5d1b4e0abd3b96763df773c460603b65
vvvvvvvesna <vvvvvvvesna@gmail.com>
2022-10-18 01:43:52 +0300

    new version 2022

--- index.js
+++ index.js
@@ -1,51 +1,29 @@
 function getDifference() {
-    var date1 = new Date('7/11/2010');
-    var date2 = new Date('12/12/2010');
-    return date2.getDate() - date1.getDate();
+  var date1 = new Date("7/11/2010");
+  var date2 = new Date("12/12/2010");
+  return date2.getDate() - date1.getDate();
 }
 
 function div(val, by) {
-    return (val - val % by) / by;
+  return (val - (val % by)) / by;
 }
 
 function getCount(start, id, final) {
-    if (final) {
-        var distance = final - start;
-    } else {
-        var now = new Date().getTime();
-        var distance = now - start;
-    }
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById(id).innerHTML =`${days} d ${hours} h ${minutes} m ${seconds} s (${days >= 365 ? div(days, 365) : 0} y ${div(days % 365, 30)} m)`;
-}
-
-function fillSkills() {
-    var ctx = document.getElementById('my-skills').getContext('2d');
-    var color = Chart.helpers.color;
-    var myChart = new Chart(ctx, {
-        type: 'radar',
-        data: {
-            labels: ['Patient', 'Friendly', 'Eating', 'Happiness', 'Motivation', 'Hard Work', 'Curiosity', 'Sport', 'Politness'],
-            datasets: [{
-                label: "Me",
-                data: [8, 5, 10, 6, 8, 9, 7, 8, 4],
-                backgroundColor: color("rgb(83, 66, 108)").alpha(0.6).rgbString(),
-            }]
-        },
-        options: {
-            scale: { ticks: { beginAtZero: true, max: 10 } },
-            elements: {
-                point: {
-                    radius: 0
-                }
-            }
-        }
-    });
-
-    myChart.options.legend.display = false;
+  if (final) {
+    var distance = final - start;
+  } else {
+    var now = new Date().getTime();
+    var distance = now - start;
+  }
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById(
+    id
+  ).innerHTML = `${days} d ${hours} h ${minutes} m ${seconds} s (${
+    days >= 365 ? div(days, 365) : 0
+  } y ${div(days % 365, 30)} m)`;
 }
 
 const startWMReply = new Date("Jan 23, 2019 10:00:00").getTime();
@@ -53,7 +31,5 @@ const startTaex = new Date("Dec 20, 2018 10:00:00").getTime();
 const finalTaex = new Date("Jul 23, 2019 10:00:00").getTime();
 getCount(startTaex, "taex", finalTaex);
 setInterval(function () {
-    getCount(startWMReply, "wm-reply");
-}, 1000)
-
-fillSkills()
+  getCount(startWMReply, "wm-reply");
+}, 1000);
