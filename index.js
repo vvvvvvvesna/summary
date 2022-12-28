0a0564bedfedffe21a144a343c1bf60246844c3a
vvvvvvvesna <vvvvvvvesna@gmail.com>
2022-12-29 01:10:25 +0300

    improve data counting

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
-function getDifference() {
-  var date1 = new Date("7/11/2010");
-  var date2 = new Date("12/12/2010");
-  return date2.getDate() - date1.getDate();
-}
-
 function div(val, by) {
   return (val - (val % by)) / by;
 }
 
-function getCount(start, id, final) {
-  if (final) {
-    var distance = final - start;
+function getCount(work) {
+  if (!work.isOngoing) {
+    var distance = work.final - work.start;
   } else {
     var now = new Date().getTime();
-    var distance = now - start;
+    var distance = now - work.start;
   }
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   document.getElementById(
-    id
-  ).innerHTML = `${days} d ${hours} h ${minutes} m ${seconds} s (${
-    days >= 365 ? div(days, 365) : 0
+    work.name
+  ).innerHTML = `${days} d ${hours} h ${minutes} m ${seconds} s (${days >= 365 ? div(days, 365) : 0
   } y ${div(days % 365, 30)} m)`;
 }
 
-const startWMReply = new Date("Jan 23, 2019 10:00:00").getTime();
-const startTaex = new Date("Dec 20, 2018 10:00:00").getTime();
-const finalTaex = new Date("Jul 23, 2019 10:00:00").getTime();
-getCount(startTaex, "taex", finalTaex);
-setInterval(function () {
-  getCount(startWMReply, "wm-reply");
-}, 1000);
+const worksArray = [{
+  isOngoing: true,
+  start: new Date("Dec 23, 2022 10:00:00").getTime(),
+  name: "freelance"
+},
+{
+  isOngoing: false,
+  start: new Date("Jan 23, 2019 10:00:00").getTime(),
+  final: new Date("Dec 23, 2022 10:00:00").getTime(),
+  name: "wm-reply"
+},
+{
+  isOngoing: false,
+  start: new Date("Dec 20, 2018 10:00:00").getTime(),
+  final: new Date("Jul 23, 2019 10:00:00").getTime(),
+  name: "taex"
+}]
+
+worksArray.forEach(work => {
+  if (work.isOngoing) {
+    getCount(work);
+    setInterval(function () {
+      getCount(work);
+    }, 1000);
+  } else
+    getCount(work);
+});
+
